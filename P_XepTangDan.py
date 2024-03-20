e134834164b1c0f515c2025f800fe7a576463b21
TamNhien <canhthien91@gmail.com>
2024-03-20 19:39:33 +0700

    Add project files.

---
+++ P_XepTangDan.py
@@ -0,0 +1,9 @@
+﻿# Nhập vào ba số a, b, c và chuyển chúng thành kiểu float
+a, b, c = map(float, input("Nhập ba số a, b, c: ").split())
+
+# Sắp xếp ba số theo thứ tự tăng dần và lưu kết quả vào biến sorted_numbers
+sorted_numbers = sorted([a, b, c])
+
+# In ra ba số đã sắp xếp theo thứ tự tăng dần, sử dụng unpacking để in ra từng số một
+print("Ba số đã sắp xếp theo thứ tự tăng dần:", *sorted_numbers)
+
